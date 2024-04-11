4ac4c6018e6c8e30f9cd04c64000aab6893275e9
Ryan Hoofard <42755382+rhoofard@users.noreply.github.com>
2024-04-11 11:39:26 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-module "sub_module" {
- source = "git@github.com:Pactionly/sample-tf-module.git?ref=v1.1.0"
+module "source_module2" {
+ source = "git@github.com:rhoofard/sample-tf-module2.git?ref=v1.0.0"
  bloop = "jibberis"
  bleep = 0
 }
